d8f1241f815eb319fa324f3d64c3cff47d174666
Rodrigo Nunes <40876810+rmnunes@users.noreply.github.com>
2023-07-10 14:04:07 -0400

    Delete run.sh

--- .github/workflows/run.sh
+++
@@ -1,7 +0,0 @@
-for f in $1
-do
-  # extension="${f##*.}"
-  filename="${f%.*}"
-  echo "Converting $f to $filename.pdf"
-  `pandoc -N --variable "geometry=margin=0.8in" "$f" --pdf-engine=xelatex -o "$filename.pdf"`
-done
