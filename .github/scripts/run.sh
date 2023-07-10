fa17fda075dc7d134c71a3039b55c6d9d5186d06
Rodrigo Nunes <40876810+rmnunes@users.noreply.github.com>
2023-07-10 14:03:18 -0400

    Update run.sh

--- .github/scripts/run.sh
+++ .github/scripts/run.sh
@@ -1,1 +1,7 @@
-
+for f in $1
+do
+  # extension="${f##*.}"
+  filename="${f%.*}"
+  echo "Converting $f to $filename.pdf"
+  `pandoc -N --variable "geometry=margin=0.8in" "$f" --pdf-engine=xelatex -o "$filename.pdf"`
+done
